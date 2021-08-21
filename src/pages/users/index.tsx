98d967adf47f67006c71bf07224183cfe5e3c8f4
Matheus Baron <matheusbaron10@gmail.com>
2021-08-21 12:01:07 -0300

    Feat: responsivity added to create user page

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../../components/Header";
-import { Sidebar } from "../../components/Sidebar";
+import { Sidebar } from "../../components/SideBar";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { 
   Box, 
