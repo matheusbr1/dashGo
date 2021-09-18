7c2ee99c12977ea4f96cb5f99f07ee0608f66651
Matheus Baron <matheusbaron10@gmail.com>
2021-09-18 12:16:43 -0300

    Feat: service hooks added

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -21,29 +21,11 @@ import {
 } from "@chakra-ui/react";
 import { Pagination } from "../../components/Pagination";
 import Link from "next/link";
-import { useQuery } from 'react-query'
-import api from '../../services/api'
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
   // users é a chave no cache
-  const { data, isLoading, error, isFetching } = useQuery('users', async () => {
-    const { data } = await api.get('users')
-
-    const users = data.users.map(user => ({
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-        day: '2-digit',
-        month: 'long',
-        year: 'numeric'
-      })
-    }))
-
-    return users
-  }, {
-    staleTime: 1000 * 5
-  })
+  const { data, isLoading, error, isFetching } = useUsers()
 
   console.log(data)
 
